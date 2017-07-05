061ce562ea11a6dd3572fd73fd55dc503ace62db
Emil Tejeda <emiltejeda@gmail.com>
2017-07-05 15:47:09 -0400

    delete debuger

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ module.exports = {
   Experiment: require("./lib/Experiment"),
   Variant: require("./lib/Variant"),
   emitter: require("./lib/emitter"),
-  experimentDebugger: require("./lib/debugger"),
-  mixpanelHelper: require("./lib/helpers/mixpanel"),
-  segmentHelper: require("./lib/helpers/segment")
 };
